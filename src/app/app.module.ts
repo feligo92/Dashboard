6ae328a4d1732be1ef09296b17421d65de04584d
Hector <hefeligo@gmail.com>
2019-11-05 10:30:04 +0100

    Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './vistas/home/home.component';
+import { ClientAreaComponent } from './vistas/client-area/client-area.component';
+import { LoginComponent } from './vistas/login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ClientAreaComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
